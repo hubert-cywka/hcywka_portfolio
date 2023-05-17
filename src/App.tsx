1ef19f290ecb077ceb5244b1301c143b5f5366ed
Hubert Cywka <hcywka@gmail.com>
2023-05-17 08:13:35 +0200

    P-8: Added vh and vw updater

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import AppProvider from './AppProvider';
 import { Container } from '@mui/material';
 import { RouterProvider } from 'react-router';
 import { AppRouter } from './router/AppRouter';
+import {
+  setCustomViewportHeightVariable,
+  setCustomViewportSizeVariableUpdater,
+  setCustomViewportWidthVariable
+} from './utility/viewportHeightUtils';
 
 function App() {
+  setCustomViewportWidthVariable();
+  setCustomViewportHeightVariable();
+  setCustomViewportSizeVariableUpdater();
+
   return (
     <AppProvider>
       <Container maxWidth="xl" sx={{ overflowX: 'hidden' }}>
---
+++ src/utility/viewportHeightUtils.tsx
@@ -0,0 +1,14 @@
+export const setCustomViewportHeightVariable = () => {
+  document.documentElement.style.setProperty('--trueViewportHeight', `${window.innerHeight}px`);
+};
+
+export const setCustomViewportWidthVariable = () => {
+  document.documentElement.style.setProperty('--trueViewportWidth', `${window.innerWidth}px`);
+};
+
+export const setCustomViewportSizeVariableUpdater = () => {
+  window.addEventListener('resize', () => {
+    setCustomViewportHeightVariable();
+    setCustomViewportWidthVariable();
+  });
+};
